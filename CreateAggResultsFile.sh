e0bf48b3a30a5e95339d0b87d440fa347e8907b2
brh22 <brh22@rstudio.cs.calvin.edu>
2019-08-07 13:48:22 -0400

    Adds "year" to input files.

--- CreateAggResultsFile.sh
+++ CreateAggResultsFile.sh
@@ -4,20 +4,19 @@
 #####################################################################################
 # Concat all of the output files that have been run for one simulation.
 #####################################################################################
+year="2014"
 data="Data"
 results="FinalResults"
-
-
-cd "$data/$results"
-
 table=Output
 filename=AggResults01pct.csv
+
+cd "$data/$results"
 
 ls *$table*.csv| awk '{if ($2 ~ /Scenario/) print $1; else print $1 " " $2}' > temp1
 
 input="temp1"
 
-echo "county,table,sector,description,direct,indirect,induced,total" > $filename
+echo "county,year,table,sector,description,direct,indirect,induced,total" > $filename
 
 # the 'while, do, done < "$input" formula reads in a file line by line and saves the line in the local variable indicated, e.g. county' 
 # awk prints (or does other processes) particular parts of lines from a file 
@@ -29,7 +28,7 @@ do
 	for table in Output Employment 
 	do
 		cat "$county"*"$table".csv | \
-		  awk -v var1="$county" -v var2="$table" '{print var1 "," var2 "," $0}' | \
+		  awk -v var1="$county" -v var2="$year" -v var3="$table" '{print var1 "," var2 "," var3 "," $0}' | \
 			sed -n -e '3,14p' >> $filename
 	done
 
--- CreateAggTopTenIndustryFile.sh
+++ CreateAggTopTenIndustryFile.sh
@@ -4,17 +4,16 @@
 #####################################################################################
 # Concat all of the output files that have been run for one simulation.
 #####################################################################################
+year="2014"
 data="Data"
 results="FinalResults"
-setup="IMPLAN Simulation Set-up Files/IMPLAN County Overview Files/csv files"
+setup="IMPLAN Simulation Set-up Files/IMPLAN Overview TopTen by Output/csv"
+input="MIcounties.csv"
+filename="AggTopTenIndustryFileOutput$year.csv"
 
 cd "$data/$setup"
 
-input="MIcounties.csv"
-
-filename="AggTopTenIndustryFile.csv"
-
-echo "county,industry,description,notused1,notused2,employment,laborinc,output" > $filename
+echo "county,year,industry,description,notused1,notused2,employment,laborinc,output" > $filename
 
 # the 'while, do, done < "$input" formula reads in a file line by line and saves the line in the local variable indicated, e.g. county' 
 # awk prints (or does other processes) particular parts of lines from a file 
@@ -24,7 +23,7 @@ echo "county,industry,description,notused1,notused2,employment,laborinc,output" 
 while IFS= read -r county
 do
 		cat *"$county"*csv | \
-		awk -v var1="$county" '{print var1 "," $0}' | \
+		awk -v var1="$county" -v var2="$year" '{print var1 "," var2 "," $0}' | \
 		sed -n -e '25,34p' >> $filename
 		
 done < "$input"
