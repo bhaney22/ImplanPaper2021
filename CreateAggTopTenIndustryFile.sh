395309dc9dacb98fe8bd7523fad20799b9081679
Becky Roselius Haney <bhaney@calvin.edu>
2019-08-07 15:45:31 -0400

    update to bash script for windows environment.

--- CreateAggTopTenIndustryFile.sh
+++ CreateAggTopTenIndustryFile.sh
@@ -4,27 +4,26 @@
 #####################################################################################
 # Concat all of the output files that have been run for one simulation.
 #####################################################################################
-year="2014"
-data="Data"
-results="FinalResults"
-setup="IMPLAN Simulation Set-up Files/IMPLAN Overview TopTen by Output/csv"
-input="MIcounties.csv"
-filename="AggTopTenIndustryFileOutput$year.csv"
 
-cd "$data/$setup"
+year="2014"    ### SET THE YEAR for the Simulation
 
-echo "county,year,industry,description,notused1,notused2,employment,laborinc,output" > $filename
+g="/mnt/c/Users/brh22/Google Drive"
+IMPLAN="_ Energy Economics/Sabbatical Project/KN - Implan Research"
+setup="IMPLAN Simulation Set-up Files/$year/IMPLAN Overview Files TopTen by Output/csv"
 
-# the 'while, do, done < "$input" formula reads in a file line by line and saves the line in the local variable indicated, e.g. county' 
-# awk prints (or does other processes) particular parts of lines from a file 
-# sed picks particular lines 
-# cut picks particular fields (columns) 
+rhome="/mnt/c/Users/brh22/Documents/R Projects/Implan Paper"
+input="$rhome/MIcounties.csv"
+outfilename="$rhome/AggTopTenIndustryFileOutput$year.csv"
+
+cd "$g/$IMPLAN/$setup"
+echo "county,year,industry,description,notused1,notused2,\
+employment,laborinc,output" > $outfilename
 
 while IFS= read -r county
 do
 		cat *"$county"*csv | \
 		awk -v var1="$county" -v var2="$year" '{print var1 "," var2 "," $0}' | \
-		sed -n -e '25,34p' >> $filename
+		sed -n -e '25,34p' >> $outfilename
 		
 done < "$input"
 
